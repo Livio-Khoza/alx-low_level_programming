7339a9da94b3607973d5fc9d5e51fce3af552642
Livio-Khoza <lv.khoza48@gmail.com>
2023-10-15 13:50:27 -0700

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * print_most_numbers - print num except 2 and 4
  *
@@ -11,8 +10,8 @@ void print_most_numbers(void)
 
 	for (c = '0'; c <= '9'; c++)
 	{
-			if (!(c == '2' || c == '4'))
-				_putchar(c);
+		if (!(c == '2' || c == '4'))
+			_putchar(c);
 	}
 	_putchar('\n');
 }

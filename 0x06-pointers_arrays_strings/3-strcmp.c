b107f4cfd0b25d927402b97dcc81e082c88c1816
Livio-Khoza <lv.khoza48@gmail.com>
2023-10-19 16:47:40 -0700

    0x06-pointers_arrays_strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strcmp - concatenates two strings
+ * @s1: pointer to first string
+ * @s2: pointer to second string
+ * if str1 == str2, 0
+ * if str1 > str2, the positive difference
+ * Return: if str1 < str2, the negetive difference
+ */
+int _strcmp(char *s1, char *s2)
+{
+        while (*s1 && *s2 && *s1 == *s2)
+        {
+                s1++;
+                s2++;
+        }
+        return (*s1 - *s2);
+}

202ee93590d7f633b8859ca6f9480c0e3721fbd4
Livio-Khoza <lv.khoza48@gmail.com>
2023-10-19 17:34:21 -0700

    0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,21 +7,21 @@
  */
 char *leet(char *s)
 {
-        char a[] = "aeotlAEOTL";
-        char n[] = "4307143071";
-        int i = 0;
-        int j;
+	char a[] = "aeotlAEOTL";
+	char n[] = "4307143071";
+	int i = 0;
+	int j;
 
-        while (*(s + i) != '\0')
-        {
-                for (j = 0; j <= 9; j++)
-                {
-                        if (*(s + i) == a[j])
-                        {
-                                *(s + i) = n[j];
-                        }
-                }
-                i++;
-        }
-        return (s);
+	while (*(s + i) != '\0')
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			if (*(s + i) == a[j])
+			{
+				*(s + i) = n[j];
+			}
+		}
+		i++;
+	}
+	return (s);
 }

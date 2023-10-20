3f2336634a85bdf9bb5d4c1480c8fd7c519a244e
Livio-Khoza <lv.khoza48@gmail.com>
2023-10-19 17:08:39 -0700

    0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -15,6 +15,6 @@ char *_strncpy(char *dest, char *src, int n)
 	for (index = 0; src[index] && index < n; index++)
 		dest[index] = src[index];
 	for (index = src_len; index < n; index++)
-		dest[index] = '\0'
-			return (dest);
+		dest[index] = '\0';
+	return (dest);
 }

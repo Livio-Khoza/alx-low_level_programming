4eea1f62c43019ab107b1dfb10198549b702d15a
Livio-Khoza <lv.khoza48@gmail.com>
2023-10-26 14:46:20 -0700

    0x08-recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen_recursion - Return the string length
+ * @s: the measured string
+ * Return: the strings lengts
+ */
+int _strlen_recursion(char *s)
+{
+        int longit = 0;
+
+        if (*s)
+        {
+                longit++;
+                longit += _strlen_recursion(s + 1);
+        }
+        return (longit);
+}
